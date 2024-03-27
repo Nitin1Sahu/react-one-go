c4fbcb5349dc0c3c3ae2e8828f637397a64a6db1
Nitin Sahu <sahunitin970@gmail.com>
2024-03-27 20:59:42 +0530

    tailwind and props dealing

---
+++ 03tailwindprops/src/App.jsx
@@ -0,0 +1,13 @@
+import './App.css'
+import Card from "./components/Card"
+
+function App() {
+  const username = "Nitin Sahu";
+  return (
+    <>
+      <Card username={username} />
+    </>
+  )
+}
+
+export default App
